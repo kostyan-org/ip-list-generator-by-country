7b21b2291128b8459c39942ea9dca728121e6ce6
Your Name <you@example.com>
2022-03-29 00:58:38 +0400

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Country IP List Generator
+[![Version](http://poser.pugx.org/kostyan-org/ip-list-generator-by-country/version)](https://packagist.org/packages/kostyan-org/ip-list-generator-by-country)
+[![Total Downloads](http://poser.pugx.org/kostyan-org/ip-list-generator-by-country/downloads)](https://packagist.org/packages/kostyan-org/ip-list-generator-by-country)
+[![License](http://poser.pugx.org/kostyan-org/ip-list-generator-by-country/license)](https://packagist.org/packages/kostyan-org/ip-list-generator-by-country)
+[![PHP Version Require](http://poser.pugx.org/kostyan-org/ip-list-generator-by-country/require/php)](https://packagist.org/packages/kostyan-org/ip-list-generator-by-country)
+
+![Image](https://github.com/kostyan-org/ip-list-generator-by-country/raw/gh-pages/ip-list-generator-by-country.PNG)
+
+[Home page](https://kostyan-org.github.io/ip-list-generator-by-country)
+
+This application is written in PHP and is used to generate a list of IP addresses of countries.
+The main purpose and data output format is for the [qBittorrent](https://www.qbittorrent.org) torrent client
+
+This site or product includes IP2Location LITE data available from [https://lite.ip2location.com](https://lite.ip2location.com).
+
+install:
+
+    composer create-project kostyan-org/ip-list-generator-by-country
+
+run:
+
+    php ./ip-list-generator-by-country/run.php generate
+
---
+++ tests/Command/GenerateCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use App\Command\GenerateCommand;
+
+class GenerateCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+        $application->add(new GenerateCommand);
+
+        /** @var GenerateCommand $command */
+        $command = $application->find('generate');
+        $commandTester = new CommandTester($command);
+
+        $inputInclud = 'included';
+        $inputCountries = ['BY'];
+        $commandTester->setInputs(['yes', 'yes', implode(',', $inputCountries), $inputInclud]);
+
+        $commandTester->execute(['command' => $command->getName()]);
+        $commandTester->assertCommandIsSuccessful();
+
+        $file = $command->getOutputFile($inputInclud, $inputCountries);
+        $this->assertFileExists($file);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('[BY] - Belarus', $output);
+        $this->assertMatchesRegularExpression('/\[([^0]|[1-9][0-9]+)\] lines/', $output);
+        $this->assertStringContainsString($file, $output);
+        $this->assertStringContainsString('Duration', $output);
+    }
+}
